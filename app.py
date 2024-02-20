a385af0913341ee9eaadc9f78060ba9568136896
Ivan Dyominov <dema.ivan@gmail.com>
2024-02-20 21:43:35 +0200

    update

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ app = Flask(__name__)
 model_total = load('model_total.joblib')
 model_home = load('model_home.joblib')
 model_away = load('model_away.joblib')
-model_handicap = load('model_handicap.joblib')
+model_firstQuarterAwayScore = load('model_firstQuarterAwayScore.joblib')
+model_firstQuarterHomeScore = load('model_firstQuarterHomeScore.joblib')
+model_secondQuarterAwayScore = load('model_secondQuarterAwayScore.joblib')
+model_secondQuarterHomeScore = load('model_secondQuarterHomeScore.joblib')
+model_thirdQuarterAwayScore = load('model_thirdQuarterAwayScore.joblib')
+model_thirdQuarterHomeScore = load('model_thirdQuarterHomeScore.joblib')
+model_fourthQuarterAwayScore = load('model_fourthQuarterAwayScore.joblib')
+model_fourthQuarterHomeScore = load('model_fourthQuarterHomeScore.joblib')
 one_hot_encoder = load('one_hot_encoder.joblib')
 
 @app.route('/predict', methods=['POST'])
@@ -16,6 +23,9 @@ def predict():
     data = request.json
     away_team = data['awayTeam']
     home_team = data['homeTeam']
+    
+    # Название матча
+    match_name = f"{home_team} vs {away_team}"
     
     # Подготовка данных для прогнозирования
     teams_for_prediction = pd.DataFrame({
@@ -25,19 +35,25 @@ def predict():
     encoded_teams = one_hot_encoder.transform(teams_for_prediction).toarray()
     encoded_teams_df = pd.DataFrame(encoded_teams, columns=one_hot_encoder.get_feature_names_out())
 
-    # Делаем предсказания
-    total_score_pred = model_total.predict(encoded_teams_df)[0]
-    home_score_pred = model_home.predict(encoded_teams_df)[0]
-    away_score_pred = model_away.predict(encoded_teams_df)[0]
-    handicap_pred = model_handicap.predict(encoded_teams_df)[0]
+    # Делаем предсказания и формируем ответ
+    predictions = [
+        {'Match': match_name},
+        {'Total Score': model_total.predict(encoded_teams_df)[0]},
+        {'Home Score': model_home.predict(encoded_teams_df)[0]},
+        {'Away Score': model_away.predict(encoded_teams_df)[0]},
+        {'1st Quarter Home Score': model_firstQuarterHomeScore.predict(encoded_teams_df)[0]},
+        {'1st Quarter Away Score': model_firstQuarterAwayScore.predict(encoded_teams_df)[0]},
+        {'2nd Quarter Home Score': model_secondQuarterHomeScore.predict(encoded_teams_df)[0]},
+        {'2nd Quarter Away Score': model_secondQuarterAwayScore.predict(encoded_teams_df)[0]},
+        {'3rd Quarter Home Score': model_thirdQuarterHomeScore.predict(encoded_teams_df)[0]},
+        {'3rd Quarter Away Score': model_thirdQuarterAwayScore.predict(encoded_teams_df)[0]},
+        {'4th Quarter Home Score': model_fourthQuarterHomeScore.predict(encoded_teams_df)[0]},
+        {'4th Quarter Away Score': model_fourthQuarterAwayScore.predict(encoded_teams_df)[0]}
+    ]
 
     # Возвращаем результаты
-    return jsonify({
-        'total_score': total_score_pred,
-        'home_score': home_score_pred,
-        'away_score': away_score_pred,
-        'handicap': handicap_pred
-    })
+    return jsonify(predictions)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
